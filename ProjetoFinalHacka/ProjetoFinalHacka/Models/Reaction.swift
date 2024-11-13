5cfcc21a69187a4b8d1871171265fa3c616ad5fc
Alessandro Souza <alessandrojsouza@gmail.com>
2024-11-13 16:28:24 -0300

    ajuste modelo e json

--- ProjetoFinalHacka/ProjetoFinalHacka/Models/Reaction.swift
+++ ProjetoFinalHacka/ProjetoFinalHacka/Models/Reaction.swift
@@ -7,11 +7,44 @@
 
 import Foundation
 
-struct Reaction: Encodable, Decodable, HashableS {
+struct User: Encodable, Decodable, Hashable {
     var _id: String
     var _rev: String
-    //var text: String
-    var user: User
-    var moment: Moment  
-    var createdat: String
+    var name: String
+    var email: String
+    var image: String
+    var password: String
+    var families: [Family]
+    
+    // Estrutura Family aninhada dentro de User
+    struct Family: Encodable, Decodable, Hashable {
+        var _id: String
+        var nome: String
+    }
+    
+    // Estrutura Coment aninhada dentro de User
+    struct Coment: Encodable, Decodable, Hashable {
+        var _id: String
+        var text: String
+        var user: User // Relacionamento de volta para User
+        var createdAt: String
+    }
+    
+    // Estrutura Moment aninhada dentro de User
+    struct Moment: Encodable, Decodable, Hashable {
+        var _id: String
+        var text: String
+        var image: String
+        var location: String
+        var createdAt: String
+        var coments: [Coment] // Relacionamento com Coment
+        var reactions: [Reaction] // Relacionamento com Reaction
+    }
+    
+    // Estrutura Reaction aninhada dentro de User
+    struct Reaction: Encodable, Decodable, Hashable {
+        var _id: String
+        var user: User // Relacionamento de volta para User
+        var createdAt: String
+    }
 }
